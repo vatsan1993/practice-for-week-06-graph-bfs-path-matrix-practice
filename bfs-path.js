0d56f3ff85d5b93f0f9ea95dafd2f041f7ed6de4
Srivatsan <vatsan1993@gmail.com>
2024-01-11 19:13:23 +0530

    finished code

--- bfs-path.js
+++ bfs-path.js
@@ -29,16 +29,41 @@ function findNeighbors(node, matrix) {
 
 function bfsPath(matrix, startNode, endValue) {
   // Your code here
+  let queue = [startNode];
+  let path = [];
+  let visited = new Set();
+  while (queue.length != 0) {
+    let currentNode = queue.shift();
+    if (!visited.has(currentNode.toString())) {
+      path.push(currentNode);
+      visited.add(currentNode.toString());
+    }
+
+    let row = currentNode[0];
+    let col = currentNode[1];
+    let val = matrix[row][col];
+    if (val === endValue) {
+      return path;
+    }
+
+    let neighbors = findNeighbors(currentNode, matrix);
+    for (let neighbor of neighbors) {
+      if (!visited.has(neighbor.toString())) {
+        queue.push(neighbor);
+      }
+    }
+  }
+  return false;
 }
 
 // ***** UNCOMMENT FOR LOCAL TESTING *****
 
-// const matrix1 = [
-//     [  1,  2,  3,  4 ],
-//     [  5,  6,  7,  8 ],
-//     [  9, 10, 11, 12 ],
-//     [ 13, 14, 15, 16 ]
-// ];
+const matrix1 = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
 
 // // EXAMPLE TESTS #1. Tests for findNeighbors function
 // console.log(findNeighbors([1,1], matrix1)) // Finds all 4 neighbors from an
@@ -53,7 +78,7 @@ function bfsPath(matrix, startNode, endValue) {
 
 // EXAMPLE TESTS #2. Tests for bfsPath function
 
-// console.log(bfsPath(matrix1, [0,0], 16)); // can traverse the entire matrix
+console.log(bfsPath(matrix1, [0, 0], 16)); // can traverse the entire matrix
 // returns an array of coordinates with no duplicates:
 
 // [
@@ -70,15 +95,15 @@ function bfsPath(matrix, startNode, endValue) {
 // Note for debugging purposes: The coordinates should represent the following matrix values, in order:
 // 1 5 2 9 6 3 13 10 7 4 14 11 8 15 12 16
 
-// console.log(bfsPath(matrix1, [2,2], 11)); // returns a single node if end
+console.log(bfsPath(matrix1, [2, 2], 11)); // returns a single node if end
 // // value is located at start node
 // // [ [ 2, 2 ] ]
 
-// console.log(bfsPath(matrix1, [1,2], 8)); // can handle various start nodes
+console.log(bfsPath(matrix1, [1, 2], 8)); // can handle various start nodes
 // // and end values
 // // [ [ 1, 2 ], [ 0, 2 ], [ 2, 2 ], [ 1, 1 ], [ 1, 3 ] ]
 
-// console.log(bfsPath(matrix1, [0,0], 17)); // can return false if end value
+console.log(bfsPath(matrix1, [0, 0], 17)); // can return false if end value
 // // is not found
 // // false
 
